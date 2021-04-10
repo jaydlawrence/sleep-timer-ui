d323cd4aaed18c9b0c42a897bb9f6113f8df71f5
Jayd Lawrence <jlawrence@questmindshare.com>
2021-04-10 08:43:38 -0300

    Fix "sleep until" bug that will set time next day if after midnight

--- src/components/sleepUntil.js
+++ src/components/sleepUntil.js
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 /** @jsx jsx */
 /** @jsxFrag React.Fragment */
 import { jsx } from '@emotion/core';
 import { MarginedButton } from './marginedButton';
-import {
-  TimePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
+import { TimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
 import moment from 'moment';
 import { formatTimeToFriendly } from '../utlis';
@@ -16,19 +13,25 @@ import { HTTP_ERROR_MESSAGE, URL_STUB_READY } from '../constants';
 import { postSetEndTime } from '../requests';
 import { CircularProgress } from '@material-ui/core';
 
+const getNextTime = (time) => {
+  if (time.isBefore(moment())) return moment(time).add(1, 'day');
+  return time;
+};
+
+const initialTime = moment();
+initialTime.set('hour', 7);
+initialTime.set('minute', 0);
+initialTime.set('second', 0);
+
 export const SleepUntil = ({ getAndUpdateState }) => {
-  const initialTime = moment();
-  initialTime.set('hour', 7);
-  initialTime.set('minute', 0);
-  initialTime.set('second', 0);
-  const [time, setTime] = useState(initialTime.add(1, 'day'));
+  const [pickerTime, setPickerTime] = useState(initialTime);
+  const [time, setTime] = useState(getNextTime(initialTime));
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const modifyAndSaveTime = newTime => {
-    if (newTime.isBefore(moment())) return setTime(newTime.add(1, 'day'));
-    return setTime(newTime);
-  }
+  const modifyAndSaveTime = (newTime) => {
+    return setPickerTime(newTime);
+  };
 
   const submitSleep = async () => {
     setIsLoading(true);
@@ -36,35 +39,36 @@ export const SleepUntil = ({ getAndUpdateState }) => {
     await getAndUpdateState();
     setIsLoading(false);
     if (result.error) setError(HTTP_ERROR_MESSAGE);
-  }
+  };
+
+  useEffect(() => {
+    setTime(getNextTime(pickerTime));
+  }, [pickerTime]);
 
   return (
     <>
       <div css={{ margin: '25px 0' }}>
         <MuiPickersUtilsProvider utils={MomentUtils}>
-          <TimePicker value={time} onChange={modifyAndSaveTime} />
+          <TimePicker value={pickerTime} onChange={modifyAndSaveTime} />
         </MuiPickersUtilsProvider>
       </div>
       <p css={{ color: 'red' }}>{error}</p>
-      <p css={{ margin: '25px 0' }} >{`Sleep until ${formatTimeToFriendly(time)}?`}</p>
-      {
-        !isLoading &&
-        <MarginedButton
-          variant="contained"
-          onClick={submitSleep}
-        >
+      <p css={{ margin: '25px 0' }}>{`Sleep until ${formatTimeToFriendly(
+        time
+      )}?`}</p>
+      {!isLoading && (
+        <MarginedButton variant="contained" onClick={submitSleep}>
           Sleep
         </MarginedButton>
-      }
-      {
-        isLoading &&
+      )}
+      {isLoading && (
         <div css={{ margin: '21px 0' }}>
           <CircularProgress />
         </div>
-      }
+      )}
       <Link style={{ textDecoration: 'none' }} to={URL_STUB_READY}>
         <MarginedButton variant="contained">Back</MarginedButton>
       </Link>
     </>
-  )
-}
+  );
+};
